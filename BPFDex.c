97c0853083c7d584c63e0cb9dd114c899ad15a13
BPFDex <152967830+BPFDex@users.noreply.github.com>
2023-12-09 15:41:32 +0800

    Add files via upload

--- BPFDex.c
+++ BPFDex.c
@@ -9,10 +9,25 @@ struct unpacking_data
     u64 arg1;
     unsigned int arg2;
     char magic[8];
+    int by_probe;
+};
+struct CodeItem {
+  unsigned short registers_size_;          // uint16_t
+  unsigned short ins_size_;                // uint16_t
+  unsigned short outs_size_;               // uint16_t
+  unsigned short tries_size_;              // uint16_t
+  unsigned int debug_info_off_;            // uint32_t
+  unsigned int insns_size_in_code_units_;  // uint32_t
+  u64 addr;
+  // unsigned short  insns_[1];                  //uint16_t
 };
 BPF_PERF_OUTPUT(Dex_event);
+BPF_PERF_OUTPUT(CodeItem_event);
 BPF_ARRAY(UID, int, 1);
-BPF_HASH(dex_array, int,unsigned char*, 10240)
+BPF_HASH(dex_array_DexOpen, int,unsigned char*, 10240)
+BPF_HASH(dex_array_DexOpenFile, int,unsigned char*, 10240)
+BPF_HASH(dex_array_OpenMemory, int,unsigned char*, 10240)
+BPF_HASH(dex_array_DexFile, int,unsigned char*, 10240)
 int trace_DexOpen(struct pt_regs *ctx)
 {
     struct unpacking_data current_data;
@@ -37,22 +52,183 @@ int trace_DexOpen(struct pt_regs *ctx)
     int size = (int)current_data.arg2;
     bpf_probe_read_user_str(&current_data.magic, sizeof(current_data.magic),
                             (char *)(current_data.arg1));
-    Dex_event.perf_submit(ctx, &current_data, sizeof(current_data));
   //use probe_read to dump Dex data
   /*
+   current_data.by_probe=1;
    unsigned char dex_data[420];
    int index=0;
    u64 addr=current_data.arg1;
 # pragma unroll
   for(size ;size>0;size-=420){
   bpf_probe_read(&dex_data,sizeof(dex_data),addr);
-  dex_array.update(&index, &dex_data);
+  dex_array_DexOpen.update(&index, &dex_data);
   index++;
   addr+=0x1A4;
   }
   size=-size;
   bpf_probe_read(&dex_data,size,addr);
-  dex_array.update(&index, &dex_data);
+  dex_array_DexOpen.update(&index, &dex_data);
   */
+  Dex_event.perf_submit(ctx, &current_data, sizeof(current_data));
   return 0;
+}
+
+int trace_DexFile(struct pt_regs *ctx)
+{
+    struct unpacking_data current_data;
+    __builtin_memset(&current_data, 0, sizeof(current_data));
+    current_data.uid = bpf_get_current_uid_gid();
+    int key = 0;
+    int uid = 0;
+    int *uid_map = UID.lookup(&key);
+    if (uid_map)
+    {
+        uid = *uid_map;
+    }
+    if (current_data.uid != uid)
+    {
+        return 0;
+    }
+    current_data.pid = bpf_get_current_pid_tgid();
+    current_data.tgid = bpf_get_current_pid_tgid() >> 32;
+    bpf_get_current_comm(current_data.comm, sizeof(current_data.comm));
+    current_data.arg1 = (unsigned long)PT_REGS_PARM1(ctx);
+    current_data.arg2 = (unsigned int)PT_REGS_PARM2(ctx);
+    int size = (int)current_data.arg2;
+    bpf_probe_read_user_str(&current_data.magic, sizeof(current_data.magic),
+                            (char *)(current_data.arg1));
+  //use probe_read to dump Dex data
+  /*
+   current_data.by_probe=2;
+   unsigned char dex_data[420];
+   int index=0;
+   u64 addr=current_data.arg1;
+# pragma unroll
+  for(size ;size>0;size-=420){
+  bpf_probe_read(&dex_data,sizeof(dex_data),addr);
+  dex_array_DexFile.update(&index, &dex_data);
+  index++;
+  addr+=0x1A4;
+  }
+  size=-size;
+  bpf_probe_read(&dex_data,size,addr);
+  dex_array_DexFile.update(&index, &dex_data);
+  */
+  Dex_event.perf_submit(ctx, &current_data, sizeof(current_data));
+  return 0;
+}
+
+int trace_DexOpenFile(struct pt_regs *ctx)
+{
+    struct unpacking_data current_data;
+    __builtin_memset(&current_data, 0, sizeof(current_data));
+    current_data.uid = bpf_get_current_uid_gid();
+    int key = 0;
+    int uid = 0;
+    int *uid_map = UID.lookup(&key);
+    if (uid_map)
+    {
+        uid = *uid_map;
+    }
+    if (current_data.uid != uid)
+    {
+        return 0;
+    }
+    current_data.pid = bpf_get_current_pid_tgid();
+    current_data.tgid = bpf_get_current_pid_tgid() >> 32;
+    bpf_get_current_comm(current_data.comm, sizeof(current_data.comm));
+    current_data.arg1 = (unsigned long)PT_REGS_PARM1(ctx);
+    current_data.arg2 = (unsigned int)PT_REGS_PARM2(ctx);
+    int size = (int)current_data.arg2;
+    bpf_probe_read_user_str(&current_data.magic, sizeof(current_data.magic),
+                            (char *)(current_data.arg1));
+  //use probe_read to dump Dex data
+  /*
+   current_data.by_probe=3;
+   unsigned char dex_data[420];
+   int index=0;
+   u64 addr=current_data.arg1;
+# pragma unroll
+  for(size ;size>0;size-=420){
+  bpf_probe_read(&dex_data,sizeof(dex_data),addr);
+  dex_array_DexOpenFile.update(&index, &dex_data);
+  index++;
+  addr+=0x1A4;
+  }
+  size=-size;
+  bpf_probe_read(&dex_data,size,addr);
+  dex_array_DexOpenFile.update(&index, &dex_data);
+  */
+  Dex_event.perf_submit(ctx, &current_data, sizeof(current_data));
+  return 0;
+}
+
+int trace_OpenMemory(struct pt_regs *ctx)
+{
+    struct unpacking_data current_data;
+    __builtin_memset(&current_data, 0, sizeof(current_data));
+    current_data.uid = bpf_get_current_uid_gid();
+    int key = 0;
+    int uid = 0;
+    int *uid_map = UID.lookup(&key);
+    if (uid_map)
+    {
+        uid = *uid_map;
+    }
+    if (current_data.uid != uid)
+    {
+        return 0;
+    }
+    current_data.pid = bpf_get_current_pid_tgid();
+    current_data.tgid = bpf_get_current_pid_tgid() >> 32;
+    bpf_get_current_comm(current_data.comm, sizeof(current_data.comm));
+    current_data.arg1 = (unsigned long)PT_REGS_PARM1(ctx);
+    current_data.arg2 = (unsigned int)PT_REGS_PARM2(ctx);
+    int size = (int)current_data.arg2;
+    bpf_probe_read_user_str(&current_data.magic, sizeof(current_data.magic),
+                            (char *)(current_data.arg1));
+  //use probe_read to dump Dex data
+  /*
+   current_data.by_probe=4;
+   unsigned char dex_data[420];
+   int index=0;
+   u64 addr=current_data.arg1;
+# pragma unroll
+  for(size ;size>0;size-=420){
+  bpf_probe_read(&dex_data,sizeof(dex_data),addr);
+  dex_array_OpenMemory.update(&index, &dex_data);
+  index++;
+  addr+=0x1A4;
+  }
+  size=-size;
+  bpf_probe_read(&dex_data,size,addr);
+  dex_array_OpenMemory.update(&index, &dex_data);
+  */
+  Dex_event.perf_submit(ctx, &current_data, sizeof(current_data));
+  return 0;
+}
+
+int trace_ExecuteGoto(struct pt_regs *ctx)
+{
+    struct CodeItem current_data;
+    __builtin_memset(&current_data, 0, sizeof(current_data));
+    current_data.uid = bpf_get_current_uid_gid();
+    int key = 0;
+    int uid = 0;
+    int *uid_map = UID.lookup(&key);
+    if (uid_map)
+    {
+        uid = *uid_map;
+    }
+    if (current_data.uid != uid)
+    {
+        return 0;
+    }
+    u64 code_item_addr = (unsigned int)PT_REGS_PARM2(ctx);
+    bpf_probe_read_user(&current_data, sizeof(current_data),
+                        (void *)(code_item_addr));
+    u64 insns_addr = code_item_addr + 0x10;
+    current_data.addr=insns_addr;
+    CodeItem_event.perf_submit(ctx, &current_data, sizeof(current_data));
+    return 0;
 }
--- BPFDex.py
+++ BPFDex.py
@@ -1,45 +1,60 @@
 from bcc import BPF
 import argparse
+import ctypes
 
-DexOpen_sym='' #replace with your symbol
-DexFile_sym='' #replace with your symbol
-OpenMemory_sym='' #replace with your symbol
-DexOpenFile_sym='' #replace with your symbol
-DefineClass_sym='' #replace with your symbol
-LoadMethod_sym='' #replace with your symbol
-ExecuteSwith_sym='' #replace with your symbol
-ExecuteGoto_sym='' #replace with your symbol
-strstr_sym='' #replace with your symbol
-strcmp_sym='' #replace with your symbol
-strncmp_sym='' #replace with your symbol
-open_sym='' #replace with your symbol
-fopen_sym='' #replace with your symbol
-openat_sym='' #replace with your symbol
-ptrace_sym='' #replace with your symbol
-gDeggbugger_sym='' #replace with your symbol
-sys_proper_get_sym='' #replace with your symbol
-sys_proper_read_sym='' #replace with your symbol
-execve_sym='' #replace with your symbol
-access_sym='' #replace with your symbol
-dlsym_sym='' #replace with your symbol
-mprotect_sym='' #replace with your symbol
-gettimeofday_sym='' #replace with your symbol
-time_sym='' #replace with your symbol
+DexOpen_sym = ""  # replace with your symbol
+DexFile_sym = ""  # replace with your symbol
+OpenMemory_sym = ""  # replace with your symbol
+DexOpenFile_sym = ""  # replace with your symbol
+DefineClass_sym = ""  # replace with your symbol
+LoadMethod_sym = ""  # replace with your symbol
+ExecuteSwith_sym = ""  # replace with your symbol
+ExecuteGoto_sym = ""  # replace with your symbol
+strstr_sym = ""  # replace with your symbol
+strcmp_sym = ""  # replace with your symbol
+strncmp_sym = ""  # replace with your symbol
+open_sym = ""  # replace with your symbol
+fopen_sym = ""  # replace with your symbol
+openat_sym = ""  # replace with your symbol
+ptrace_sym = ""  # replace with your symbol
+gDeggbugger_sym = ""  # replace with your symbol
+sys_proper_get_sym = ""  # replace with your symbol
+sys_proper_read_sym = ""  # replace with your symbol
+execve_sym = ""  # replace with your symbol
+access_sym = ""  # replace with your symbol
+dlsym_sym = ""  # replace with your symbol
+mprotect_sym = ""  # replace with your symbol
+gettimeofday_sym = ""  # replace with your symbol
+time_sym = ""  # replace with your symbol
 
-libart_path='' #replace with your path
-libc_path='' #replace with your path
+libart_path = ""  # replace with your path
+libc_path = ""  # replace with your path
 
 
 bpf_c_code = open("BPFDex.c").read()
 bpf = BPF(text=bpf_c_code)
-dex_index=0
-size_list=[]
-addr_list=[]
+dex_index = 0
+size_list = []
+addr_list = []
+dex_array = []
+ins_array=[]
+ins_addr_list=[]
+pid=0
 
-parser = argparse.ArgumentParser(description='unpacking Android apps by eBPF')
-parser.add_argument('uid', help='uid of the target app')
-parser.add_argument('--insExtract', '-i', action='store_true', help='turn on when the packer extracts instructions of Dex')
-parser.add_argument('--behavior', '-b', type=str, default=None, help='''
+parser = argparse.ArgumentParser(description="unpacking Android apps by eBPF")
+parser.add_argument("uid", help="uid of the target app")
+parser.add_argument(
+    "--insExtract",
+    "-i",
+    action="store_true",
+    help="turn on when the packer extracts instructions of Dex",
+)
+parser.add_argument(
+    "--behavior",
+    "-b",
+    type=str,
+    default=None,
+    help="""
                     input the behavior you want to monitor
                     EMU               ---emulator detection
                     DBG               ---debugger detection
@@ -47,72 +62,166 @@ parser.add_argument('--behavior', '-b', type=str, default=None, help='''
                     TCK               ---time check
                     SLH               ---lib hook
                     RTD               ---root detection
-                    ''')
+                    """,
+)
 args = parser.parse_args()
-bpf['UID'][0]=args.uid
+bpf["UID"][0] = args.uid
+
 
 def Dex_event(cpu, data, size):
-    global size_list,addr_list
+    global size_list, addr_list,pid
     event = bpf["Dex_event"].event(data)
-    print("pid:{1}     tgid:{2}     uid:{6}    comm:{0}     arg1:{3}    arg2:{4}     magic:{5}".format(
-        event.comm, event.pid, event.tgid, event.arg1,event.arg2,event.magic,event.uid))
-    if((event.arg2 not in size_list )or(event.arg1 not in addr_list)):
-        Dump_Dex_via_mem(event.pid,event.arg1,event.arg2)
-        
-def Dump_Dex_via_mem(pid,addr,size):
-    global dex_index,size_list
-    mem=open("/proc/{0}/mem".format(pid),"rb+")
+    print(
+        "pid:{1}     tgid:{2}     uid:{6}    comm:{0}     arg1:{3}    arg2:{4}     magic:{5}".format(
+            event.comm,
+            event.pid,
+            event.tgid,
+            event.arg1,
+            event.arg2,
+            event.magic,
+            event.uid,
+        )
+    )
+    pid=event.pid
+    # if event.by_probe != 0:
+    #     Dump_Dex_via_prob(event.by_probe)
+    if (event.arg2 not in size_list) or (event.arg1 not in addr_list):
+        Dump_Dex_via_mem(event.pid, event.arg1, event.arg2)
+
+def CodeItem_event(cpu, data, size):
+    global pid
+    event = bpf["CodeItem_event"].event(data)
+    Dump_ins_via_mem(event.addr,event.ins_size_,pid)
+
+
+def Dump_Dex_via_mem(pid, addr, size):
+    global dex_index, size_list, dex_array
+    mem = open("/proc/{0}/mem".format(pid), "rb+")
     mem.seek(addr)
-    dex=open(file="{0}.dex".format(dex_index),mode="wb")
     try:
-        res=mem.read(size)
+        res = mem.read(size)
     except IOError:
         print("unable to dump")
     else:
-        print("complete {0}.dex".format(dex_index))
-        dex.write(res)
+        dex_array.append(res)
         mem.close()
-        dex.close()
-        dex_index+=1
+        dex_index += 1
         size_list.append(size)
+        addr_list.append(addr)
+        print("dump {0}.dex".format(dex_index))
+
+
+def final_Dex_process():
+    for i in range(dex_index):
+        dex = open(file="{0}.dex".format(i), mode="wb")
+        dex.write(dex_array[i])
+        dex.close()
+
+
+# def Dump_Dex_via_prob(probe_type):
+#     global dex_array,dex_index
+#     if(probe_type==1):
+#         dex_probe_array = bpf["dex_array_DexOpen"]
+#         res=b''
+#         for i in dex_probe_array:
+#             res+=i
+#         dex_array.append(res)
+#         dex_index+=1
+#         bpf["dex_array_DexOpen"].clear()
+#     elif(probe_type==2):
+#         dex_probe_array = bpf["dex_array_DexOpenFile"]
+#         res=b''
+#         for i in dex_probe_array:
+#             res+=i
+#         dex_array.append(res)
+#         dex_index+=1
+#         bpf["dex_array_DexOpenFile"].clear()
+#     elif(probe_type==2):
+#         dex_probe_array = bpf["dex_array_OpenMemory"]
+#         res=b''
+#         for i in dex_probe_array:
+#             res+=i
+#         dex_array.append(res)
+#         dex_index+=1
+#         bpf["dex_array_OpenMemory"].clear()
+#     elif(probe_type==3):
+#         dex_probe_array = bpf["dex_array_DexFile"]
+#         res=b''
+#         for i in dex_probe_array:
+#             res+=i
+#         dex_array.append(res)
+#         dex_index+=1
+#         bpf["dex_array_DexFile"].clear()
+
+def Dump_ins_via_mem(addr,size,pid):
+    global ins_array
+    if pid==0:
+        return
+    mem = open("/proc/{0}/mem".format(pid), "rb+")
+    mem.seek(addr)
+    try:
+        res = mem.read(size)
+    except IOError:
+        print("unable to dump codeitem")
+    else:
+        ins_array.append(res)
+        mem.close()
+        ins_addr_list.append(addr)
         
-def Dump_Dex_via_prob(pid,addr,size):
-    
+
 def run():
-    if(args.insExtract!=True ):
-        #DexOpen
+    if args.insExtract != True:
+        # DexOpen
         bpf.attach_uprobe(name=libart_path, sym=DexOpen_sym, fn_name="trace_DexOpen")
-        #DexFile
+        # DexFile
         bpf.attach_uprobe(name=libart_path, sym=DexFile_sym, fn_name="trace_DexFile")
-        #DexOpenFile
-        bpf.attach_uprobe(name=libart_path, sym=DexOpenFile_sym, fn_name="trace_DexOpenFile")
-        #OpenMemory
-        bpf.attach_uprobe(name=libart_path, sym=OpenMemory_sym, fn_name="trace_OpenMemory")
-    elif(args.insExtract==True ):
-        #DexOpen
+        # DexOpenFile
+        bpf.attach_uprobe(
+            name=libart_path, sym=DexOpenFile_sym, fn_name="trace_DexOpenFile"
+        )
+        # OpenMemory
+        bpf.attach_uprobe(
+            name=libart_path, sym=OpenMemory_sym, fn_name="trace_OpenMemory"
+        )
+    elif args.insExtract == True:
+        # DexOpen
         bpf.attach_uprobe(name=libart_path, sym=DexOpen_sym, fn_name="trace_DexOpen")
-        #DexFile
+        # DexFile
         bpf.attach_uprobe(name=libart_path, sym=DexFile_sym, fn_name="trace_DexFile")
-        #DexOpenFile
-        bpf.attach_uprobe(name=libart_path, sym=DexOpenFile_sym, fn_name="trace_DexOpenFile")
-        #OpenMemory
-        bpf.attach_uprobe(name=libart_path, sym=OpenMemory_sym, fn_name="trace_OpenMemory")
-        #ExecuteGoto
-        bpf.attach_uprobe(name=libart_path, sym=ExecuteGoto_sym, fn_name="trace_ExecuteGoto")
-        #ExecuteSwith
-        bpf.attach_uprobe(name=libart_path, sym=ExecuteSwith_sym, fn_name="trace_ExecuteSwith")
-    if(args.behavior!=None):
-        if(args.behavior=='EMU'):
+        # DexOpenFile
+        bpf.attach_uprobe(
+            name=libart_path, sym=DexOpenFile_sym, fn_name="trace_DexOpenFile"
+        )
+        # OpenMemory
+        bpf.attach_uprobe(
+            name=libart_path, sym=OpenMemory_sym, fn_name="trace_OpenMemory"
+        )
+        # ExecuteGoto
+        bpf.attach_uprobe(
+            name=libart_path, sym=ExecuteGoto_sym, fn_name="trace_ExecuteGoto"
+        )
+        # ExecuteSwith
+        bpf.attach_uprobe(
+            name=libart_path, sym=ExecuteSwith_sym, fn_name="trace_ExecuteSwith"
+        )
+    if args.behavior != None:
+        if args.behavior == "EMU":
             bpf.attach_uprobe(name=libc_path, sym=open_sym, fn_name="trace_open")
             bpf.attach_uprobe(name=libc_path, sym=fopen_sym, fn_name="trace_fopen")
             bpf.attach_uprobe(name=libc_path, sym=openat_sym, fn_name="trace_openat")
-            bpf.attach_uprobe(name=libc_path, sym=sys_proper_get_sym, fn_name="trace_sys_proper_get")
-            bpf.attach_uprobe(name=libc_path, sym=sys_proper_read_sym, fn_name="trace_sys_proper_read")
+            bpf.attach_uprobe(
+                name=libc_path, sym=sys_proper_get_sym, fn_name="trace_sys_proper_get"
+            )
+            bpf.attach_uprobe(
+                name=libc_path, sym=sys_proper_read_sym, fn_name="trace_sys_proper_read"
+            )
             bpf.attach_uprobe(name=libc_path, sym=strstr_sym, fn_name="trace_strstr")
             bpf.attach_uprobe(name=libc_path, sym=strcmp_sym, fn_name="trace_strcmp")
             bpf.attach_uprobe(name=libc_path, sym=strncmp_sym, fn_name="trace_strncmp")
-        if(args.behavior=='DBG'):
-            bpf.attach_uprobe(name=libc_path, sym=gDeggbugger_sym, fn_name="trace_gDebugger")
+        if args.behavior == "DBG":
+            bpf.attach_uprobe(
+                name=libc_path, sym=gDeggbugger_sym, fn_name="trace_gDebugger"
+            )
             bpf.attach_uprobe(name=libc_path, sym=ptrace_sym, fn_name="trace_ptrace")
             bpf.attach_uprobe(name=libc_path, sym=open_sym, fn_name="trace_open")
             bpf.attach_uprobe(name=libc_path, sym=fopen_sym, fn_name="trace_fopen")
@@ -120,34 +229,46 @@ def run():
             bpf.attach_uprobe(name=libc_path, sym=strstr_sym, fn_name="trace_strstr")
             bpf.attach_uprobe(name=libc_path, sym=strcmp_sym, fn_name="trace_strcmp")
             bpf.attach_uprobe(name=libc_path, sym=strncmp_sym, fn_name="trace_strncmp")
-        if(args.behavior=='DBI'):
+        if args.behavior == "DBI":
             bpf.attach_uprobe(name=libc_path, sym=open_sym, fn_name="trace_open")
             bpf.attach_uprobe(name=libc_path, sym=fopen_sym, fn_name="trace_fopen")
             bpf.attach_uprobe(name=libc_path, sym=openat_sym, fn_name="trace_openat")
             bpf.attach_uprobe(name=libc_path, sym=strstr_sym, fn_name="trace_strstr")
             bpf.attach_uprobe(name=libc_path, sym=strcmp_sym, fn_name="trace_strcmp")
             bpf.attach_uprobe(name=libc_path, sym=strncmp_sym, fn_name="trace_strncmp")
-        if(args.behavior=='TCK'):
-            bpf.attach_uprobe(name=libc_path, sym=gettimeofday_sym, fn_name="trace_gettimeofday")
+        if args.behavior == "TCK":
+            bpf.attach_uprobe(
+                name=libc_path, sym=gettimeofday_sym, fn_name="trace_gettimeofday"
+            )
             bpf.attach_uprobe(name=libc_path, sym=time_sym, fn_name="trace_time")
-        if(args.behavior=='SLH'):
+        if args.behavior == "SLH":
             bpf.attach_uprobe(name=libc_path, sym=dlsym_sym, fn_name="trace_dlsym")
-            bpf.attach_uprobe(name=libc_path, sym=mprotect_sym, fn_name="trace_mprotect")
-        if(args.behavior=='RDT'):
+            bpf.attach_uprobe(
+                name=libc_path, sym=mprotect_sym, fn_name="trace_mprotect"
+            )
+        if args.behavior == "RDT":
             bpf.attach_uprobe(name=libc_path, sym=open_sym, fn_name="trace_open")
             bpf.attach_uprobe(name=libc_path, sym=fopen_sym, fn_name="trace_fopen")
             bpf.attach_uprobe(name=libc_path, sym=openat_sym, fn_name="trace_openat")
             bpf.attach_uprobe(name=libc_path, sym=strstr_sym, fn_name="trace_strstr")
             bpf.attach_uprobe(name=libc_path, sym=strcmp_sym, fn_name="trace_strcmp")
             bpf.attach_uprobe(name=libc_path, sym=strncmp_sym, fn_name="trace_strncmp")
-            bpf.attach_uprobe(name=libc_path, sym=sys_proper_get_sym, fn_name="trace_sys_proper_get")
-            bpf.attach_uprobe(name=libc_path, sym=sys_proper_read_sym, fn_name="trace_sys_proper_read")
+            bpf.attach_uprobe(
+                name=libc_path, sym=sys_proper_get_sym, fn_name="trace_sys_proper_get"
+            )
+            bpf.attach_uprobe(
+                name=libc_path, sym=sys_proper_read_sym, fn_name="trace_sys_proper_read"
+            )
             bpf.attach_uprobe(name=libc_path, sym=execve_sym, fn_name="trace_execve")
             bpf.attach_uprobe(name=libc_path, sym=access_sym, fn_name="trace_access")
     bpf["Dex_event"].open_perf_buffer(Dex_event)
+    bpf["CodeItem_event"].open_perf_buffer(CodeItem_event)
     while True:
         bpf.perf_buffer_poll()
+
+
 try:
     run()
 except KeyboardInterrupt:
+    final_Dex_process()
     exit()
